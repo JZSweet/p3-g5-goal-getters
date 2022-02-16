adb44e40b97bf4054210be2aab57e3ac687a533b
JZ <73402906+JZSweet@users.noreply.github.com>
2022-02-16 12:44:13 -0800

    add links to navbars and conect the pages, remove unused files

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,31 @@
+// humburger menu
 var button = document.querySelector(".hamburgerMenu");
 var menu = document.querySelector("nav");
 var open = false;
-button.addEventListener("click", function () {
-  if (open) {
-    ("if the menu is closed, then open it");
-    menu.style.display = "none";
-    open = false;
-  } else {
-    ("if the menu is open, then close it");
-    menu.style.display = "inline-block";
-    open = true;
-  }
+button.addEventListener("click", function() {
+    if (open) {
+        ("if the menu is closed, then open it");
+        menu.style.display = "none";
+        open = false;
+    } else {
+        ("if the menu is open, then close it");
+        menu.style.display = "inline-block";
+        open = true;
+    }
 });
+
+// accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+};
